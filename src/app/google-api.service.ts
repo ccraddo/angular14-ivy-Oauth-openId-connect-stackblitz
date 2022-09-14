b679b211da7ae2be7223fbbb5527f38218b929e5
ccraddo <45082319+ccraddo@users.noreply.github.com>
2022-09-14 15:54:09 -0400

    1 file modified

--- src/app/google-api.service.ts
+++ src/app/google-api.service.ts
@@ -1,14 +1,28 @@
 import { Injectable } from '@angular/core';
 import { AuthConfig, OAuthService } from 'angular-oauth2-oidc';
 
-const oAuthConfig: AuthConfig= {
-  issuer:'https://accounts.google.com',
+const oAuthConfig: AuthConfig = {
+  issuer: 'https://accounts.google.com',
   strictDiscoveryDocumentValidation: false,
   redirectUri: window.location.origin,
-  clientId:'551875752318-cilvt74ecqd9k9get4j0tst1buhai967.apps.googleusercontent.com',
-  scope: 'openid profile email'
-}
+  clientId:
+    '551875752318-cilvt74ecqd9k9get4j0tst1buhai967.apps.googleusercontent.com',
+  scope: 'openid profile email',
+};
 @Injectable()
 export class GoogleApiService {
-  constructor(private readonly oAuthService: OAuthService) {}
+  constructor(private readonly oAuthService: OAuthService) {
+    oAuthService.configure(oAuthConfig);
+    oAuthService.loadDiscoveryDocument().then(() => {
+      oAuthService.tryLoginImplicitFlow().then(() => {
+        if (!oAuthService.hasValidAccessToken) {
+          oAuthService.initLoginFlow();
+        } else {
+          oAuthService.loadUserProfile().then((userProfile) => {
+            console.log(JSON.stringify(userProfile));
+          });
+        }
+      });
+    });
+  }
 }
