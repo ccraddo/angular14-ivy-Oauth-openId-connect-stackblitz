a010b1f3b779163bc28ce35d275aa0f8b2a7b7aa
ccraddo <45082319+ccraddo@users.noreply.github.com>
2022-09-14 16:04:11 -0400

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { HttpClientModule } from '@angular/common/http';
 import { OAuthModule } from 'angular-oauth2-oidc';
 
