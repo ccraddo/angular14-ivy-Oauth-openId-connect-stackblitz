5efd9c2fa194a1bf719f97f8ea7f0f9091b8603e
ccraddo <45082319+ccraddo@users.noreply.github.com>
2022-09-14 15:45:15 -0400

    1 file modified

--- src/app/google-api.service.ts
+++ src/app/google-api.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@angular/core';
-import { OAuthService } from 'angular-oauth2-oidc';
+import { AuthConfig, OAuthService } from 'angular-oauth2-oidc';
 
+const oAuthConfig: AuthConfig= {
+  issuer:'https://accounts.google.com',
+  strictDiscoveryDocumentValidation: false,
+  redirectUri: window.location.origin,
+  clientId:'551875752318-cilvt74ecqd9k9get4j0tst1buhai967.apps.googleusercontent.com',
+  scope: 'openid profile email'
+}
 @Injectable()
 export class GoogleApiService {
   constructor(private readonly oAuthService: OAuthService) {}
